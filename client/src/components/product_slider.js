cd2ae32d202ea057866b88cc56af6d49834b9b56
Atte Hinkka <atte.hinkka@iki.fi>
2017-12-26 19:21:50 +0200

    Product slider with tooltips and 0-1 positioning

--- client/src/components/product_slider.js
+++ client/src/components/product_slider.js
@@ -3,6 +3,15 @@ import {OverlayTrigger, Tooltip} from "react-bootstrap";
 
 
 class Tick extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.props.dispatch({type: this.props.action, payload: this.props.payload})
+  }
+
   render() {
     let color = "#ffffff"
     if (this.props.active == true) {
@@ -10,9 +19,16 @@ class Tick extends React.Component {
     } else if (this.props.loaded == true) {
       color = "#c0c0c0"
     }
-    
+
+    const tooltip = (
+        <Tooltip id={"tick-" + this.props.position + "-tooltip"}>{this.props.tooltip}</Tooltip>
+    )
+
     return (
-        <div style={{position: "absolute", left: this.props.position * 100 + "%", color: color}}>▎</div>
+        <OverlayTrigger placement="bottom" overlay={tooltip}>
+          <div style={{position: "absolute", left: this.props.position * 100 + "%", color: color}}
+               onClick={this.onClick}>▎</div>
+        </OverlayTrigger>
     )
   }
 }
@@ -22,12 +38,12 @@ export class ProductSlider extends React.Component {
   render() {
 
     return (
-	<div className="progress">
-  	<Tick position={0.1} loaded={true} active={true} />
-	<Tick position={0.2} loaded={true} active={false} />
-	<Tick position={0.5} loaded={true} active={false} />
-	<Tick position={0.9} loaded={false} active={false} />
-	</div>
+        <div className="progress">
+        <Tick position={0.1} loaded={true} active={true} tooltip="foo" />
+        <Tick position={0.2} loaded={true} active={false} tooltip="bar" />
+        <Tick position={0.5} loaded={true} active={false} />
+        <Tick position={0.9} loaded={false} active={false} />
+        </div>
       )
   }
 }
