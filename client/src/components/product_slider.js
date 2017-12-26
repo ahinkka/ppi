97508f5c47144fdf6dabd7f67ba1fe464a76b921
Atte Hinkka <atte.hinkka@iki.fi>
2017-12-26 19:12:36 +0200

    Product slider with ticks

--- client/src/components/product_slider.js
+++ client/src/components/product_slider.js
@@ -2,14 +2,31 @@ import React from "react"
 import {OverlayTrigger, Tooltip} from "react-bootstrap";
 
 
+class Tick extends React.Component {
+  render() {
+    let color = "#ffffff"
+    if (this.props.active == true) {
+      color = "#000000"
+    } else if (this.props.loaded == true) {
+      color = "#c0c0c0"
+    }
+    
+    return (
+        <div style={{position: "absolute", left: this.props.position * 100 + "%", color: color}}>▎</div>
+    )
+  }
+}
+
+
 export class ProductSlider extends React.Component {
   render() {
+
     return (
 	<div className="progress">
-          <div className="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"
-               style={{width: "60%"}}>
-            <span className="sr-only">60% Complete</span>
-	  </div>
+  	<Tick position={0.1} loaded={true} active={true} />
+	<Tick position={0.2} loaded={true} active={false} />
+	<Tick position={0.5} loaded={true} active={false} />
+	<Tick position={0.9} loaded={false} active={false} />
 	</div>
       )
   }
