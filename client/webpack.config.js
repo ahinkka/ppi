688a7161043fa35dd889c6e06e3524351cb4793b
Atte Hinkka <atte.hinkka@iki.fi>
2019-04-21 16:27:37 +0300

    Migrate to webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'observer.js',
+    path: path.resolve(__dirname, 'build/js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
