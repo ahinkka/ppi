b9c9bf0f661d58ac683cec4685e09859ea08b357
Atte Hinkka <atte.hinkka@iki.fi>
2021-01-07 21:15:17 +0200

    Convert ToggleButton into a functional component

--- client/src/components/toggle_button.js
+++ client/src/components/toggle_button.js
@@ -2,37 +2,28 @@ import React from 'react'
 import {OverlayTrigger, Tooltip} from 'react-bootstrap';
 
 
-export class ToggleButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-  }
+export default function(props) {
+  const tooltip = (
+    <Tooltip id="pause-tooltip">{props.tooltip}</Tooltip>
+  )
 
-  handleChange() {
-    this.props.dispatch({type: this.props.action})
-  }
-
-  render() {
-    const tooltip = (
-      <Tooltip id="pause-tooltip">{this.props.tooltip}</Tooltip>
+  if (props.toggleStatus == false) {
+    return (
+      <OverlayTrigger placement="bottom" overlay={tooltip}>
+        <button type="button" className="btn btn-primary"
+          data-toggle="button" aria-pressed="false"
+          onClick={() => props.dispatch({type: props.action})}>{props.offSymbol}
+        </button>
+      </OverlayTrigger>
     )
-
-    if (this.props.toggleStatus == false) {
-      return (
-        <OverlayTrigger placement="bottom" overlay={tooltip}>
-          <button type="button" className="btn btn-primary"
-            data-toggle="button" aria-pressed="false"
-            onClick={this.handleChange}>{this.props.offSymbol}</button>
-        </OverlayTrigger>
-      )
-    } else {
-      return (
-        <OverlayTrigger placement="bottom" overlay={tooltip}>
-          <button type="button" className="btn btn-primary active"
-            data-toggle="button" aria-pressed="true"
-            onClick={this.handleChange}>{this.props.onSymbol}</button>
-        </OverlayTrigger>
-      )
-    }
+  } else {
+    return (
+      <OverlayTrigger placement="bottom" overlay={tooltip}>
+        <button type="button" className="btn btn-primary active"
+          data-toggle="button" aria-pressed="true"
+          onClick={() => props.dispatch({type: props.action})}>{props.onSymbol}
+        </button>
+      </OverlayTrigger>
+    )
   }
 }
