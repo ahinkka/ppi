0f4ccf59137e2e7d48d8752fdabebdd9ac9fb7b8
Atte Hinkka <atte.hinkka@iki.fi>
2021-01-07 21:33:50 +0200

    Make ProductSlider functional

--- client/src/components/product_slider.js
+++ client/src/components/product_slider.js
@@ -11,48 +11,37 @@ const Tick = (props) => {
   return (
     <OverlayTrigger placement="bottom" overlay={tooltip}>
       <div style={{position: 'absolute', left: (0.01 + 0.98 * props.position) * 100 + '%',
-		   color: props.color, cursor: 'pointer'}}
+                   color: props.color, cursor: 'pointer'}}
            onClick={props.clicked}>{props.character}</div>
     </OverlayTrigger>
   )
 }
 
 
-export class ProductSlider extends React.Component {
-  constructor(props) {
-    super(props);
+export default function(props) {
+  const ticks = props.ticks.map(function(item) {
+    return (
+      <Tick key={item.key} position={item.position} color={item.color}
+            character={item.character} tooltip={item.tooltip} clicked={item.clicked} />
+    )
+  })
 
-    this.onWheel = this.onWheel.bind(this);
-    // this.initializeKeyboardListener = this.initializeKeyboardListener.bind(this);
-    // this.removeKeyboardListener = this.removeKeyboardListener.bind(this);
-  }
-
-  onWheel(event) {
+  const onWheel = (event) => {
     if (event.deltaY < 0) {
-      this.props.dispatch({type: ObserverActions.TICK_BACKWARD})
+      props.dispatch({type: ObserverActions.TICK_BACKWARD})
     } else if (event.deltaY > 0) {
-      this.props.dispatch({type: ObserverActions.TICK_FORWARD})
+      props.dispatch({type: ObserverActions.TICK_FORWARD})
     }
   }
 
-  render() {
-    const tmp = this
-    const ticks = this.props.ticks.map(function(item) {
-      return (
-        <Tick key={item.key} position={item.position} color={item.color}
-              character={item.character} tooltip={item.tooltip} clicked={item.clicked} />
-      )
-    })
-
-    return (
-      <div ref={this.containerRef} className='time-slider' onWheel={this.onWheel}>
-        <div className='time-slider__line'>
-          <hr></hr>
-        </div>
-        <div className='time-slider__ticks'>
-          { ticks }
-        </div>
+  return (
+    <div className='time-slider' onWheel={onWheel}>
+      <div className='time-slider__line'>
+        <hr></hr>
       </div>
-    )
-  }
+      <div className='time-slider__ticks'>
+        { ticks }
+      </div>
+    </div>
+  )
 }
