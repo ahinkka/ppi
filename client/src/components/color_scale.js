25bbeedd9e13bc7c7429ee5285357351b60ccec5
Atte Hinkka <atte.hinkka@iki.fi>
2021-01-11 21:05:52 +0200

    Back to functional component for color scale, with React.memo

--- client/src/components/color_scale.js
+++ client/src/components/color_scale.js
@@ -17,48 +17,45 @@ function SolidColorRange(props) {
 }
 
 
-class ColorScale extends React.PureComponent {
-  render() {
-    const props = this.props
-    // -type: ScaleRangeType.STEP
-    // -color [r,g,b]
-    // -start
-    //  -open: t/f
-    //  -value: value
-    // -end
-    //  -open: t/f
-    //  -value: value
+const MemoizedColorScale = React.memo(function ColorScale(props) {
+  // -type: ScaleRangeType.STEP
+  // -color [r,g,b]
+  // -start
+  //  -open: t/f
+  //  -value: value
+  // -end
+  //  -open: t/f
+  //  -value: value
 
-    const tmp = props.ranges.slice(0)
-    tmp.reverse()
-    const ranges = tmp.map(function(item) {
-      if (item.type !== ScaleRangeType.STEP) {
-        throw new Error('Unhandled step type: ' + item.type);
-      }
+  const tmp = props.ranges.slice(0)
+  tmp.reverse()
+  const ranges = tmp.map(function(item) {
+    if (item.type !== ScaleRangeType.STEP) {
+      throw new Error('Unhandled step type: ' + item.type);
+    }
 
-      return (
-        <SolidColorRange key={'range-' + item.start.value}
-          color={item.color}
-          start={item.start}
-          end={item.end} />
-      )
-    })
-
-    // TODO: use non-px units everywhere
-    // <div style={{width: '7em'}}>{props.name}</div><br/>
     return (
-      <Draggable grid={[25, 25]}>
-        <div style={{backgroundColor: '#c0c0c0', padding: '10px',
-          'zIndex': 100, position: 'absolute', top: 150, left: 25}}>
-          <div style={{width: '7em', fontSize: '0.9em'}}>{props.type}, {props.unit}</div>
-          <div style={{display: 'flex', flexDirection: 'column'}}>
-            {ranges}
-          </div>
-        </div>
-      </Draggable>
+      <SolidColorRange key={'range-' + item.start.value}
+        color={item.color}
+        start={item.start}
+        end={item.end} />
     )
-  }
-}
+  })
+
+  // TODO: use non-px units everywhere
+  // <div style={{width: '7em'}}>{props.name}</div><br/>
+  return (
+    <Draggable grid={[25, 25]}>
+      <div style={{backgroundColor: '#c0c0c0', padding: '10px',
+        'zIndex': 100, position: 'absolute', top: 150, left: 25}}>
+        <div style={{width: '7em', fontSize: '0.9em'}}>{props.type}, {props.unit}</div>
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+          {ranges}
+        </div>
+      </div>
+    </Draggable>
+  )
+})
 
 
-export default ColorScale
+export default MemoizedColorScale
