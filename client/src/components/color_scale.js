b086b70b096bae7d7f522b1ee85a879464a3f758
Atte Hinkka <atte.hinkka@iki.fi>
2021-01-11 20:44:37 +0200

    Don't render color scale if it hasn't changed

--- client/src/components/color_scale.js
+++ client/src/components/color_scale.js
@@ -17,44 +17,54 @@ function SolidColorRange(props) {
 }
 
 
-function ColorScale(props) {
-  // -type: ScaleRangeType.STEP
-  // -color [r,g,b]
-  // -start
-  //  -open: t/f
-  //  -value: value
-  // -end
-  //  -open: t/f
-  //  -value: value
+class ColorScale extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    return (this.props.ranges !== nextProps.ranges ||
+            this.props.type !== nextProps.type ||
+            this.props.unit !== nextProps.unit ||
+            this.props.name !== nextProps.name)
+  }
 
-  const tmp = props.ranges.slice(0)
-  tmp.reverse()
-  const ranges = tmp.map(function(item) {
-    if (item.type !== ScaleRangeType.STEP) {
-      throw new Error('Unhandled step type: ' + item.type);
-    }
+  render() {
+    const props = this.props
+    // -type: ScaleRangeType.STEP
+    // -color [r,g,b]
+    // -start
+    //  -open: t/f
+    //  -value: value
+    // -end
+    //  -open: t/f
+    //  -value: value
 
+    const tmp = props.ranges.slice(0)
+    tmp.reverse()
+    const ranges = tmp.map(function(item) {
+      if (item.type !== ScaleRangeType.STEP) {
+        throw new Error('Unhandled step type: ' + item.type);
+      }
+
+      return (
+        <SolidColorRange key={'range-' + item.start.value}
+          color={item.color}
+          start={item.start}
+          end={item.end} />
+      )
+    })
+
+    // TODO: use non-px units everywhere
+    // <div style={{width: '7em'}}>{props.name}</div><br/>
     return (
-      <SolidColorRange key={'range-' + item.start.value}
-        color={item.color}
-        start={item.start}
-        end={item.end} />
-    )
-  })
-
-  // TODO: use non-px units everywhere
-  // <div style={{width: '7em'}}>{props.name}</div><br/>
-  return (
-    <Draggable grid={[25, 25]}>
-      <div style={{backgroundColor: '#c0c0c0', padding: '10px',
-        'zIndex': 100, position: 'absolute', top: 150, left: 25}}>
-        <div style={{width: '7em', fontSize: '0.9em'}}>{props.type}, {props.unit}</div>
-        <div style={{display: 'flex', flexDirection: 'column'}}>
-          {ranges}
+      <Draggable grid={[25, 25]}>
+        <div style={{backgroundColor: '#c0c0c0', padding: '10px',
+          'zIndex': 100, position: 'absolute', top: 150, left: 25}}>
+          <div style={{width: '7em', fontSize: '0.9em'}}>{props.type}, {props.unit}</div>
+          <div style={{display: 'flex', flexDirection: 'column'}}>
+            {ranges}
+          </div>
         </div>
-      </div>
-    </Draggable>
-  )
+      </Draggable>
+    )
+  }
 }
 
 
