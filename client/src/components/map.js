0109eb1a053bc0e1b2d7a1d080de7d65b4895e52
Atte Hinkka <atte.hinkka@iki.fi>
2021-03-23 22:11:37 +0200

    Flip North and South so that cursor tool starts making sense

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -39,10 +39,10 @@ const yiqColorContrast = (r, g, b) => (r*299 + g*587 + b*114 ) / 1000.0 >= 128
 
 const bearingToCompassRoseReading = (bearing) => {
   const cardinals = [
-    ['N', 0],   ['NNE', 22.5],  ['NE',  45], ['ENE', 67.5],
-    ['E', 90],  ['ESE', 112.5], ['SE', 135], ['SSE', 157.5],
-    ['S', 180], ['SSW', 202.5], ['SW', 225], ['WSW', 247.5],
-    ['W', 270], ['WNW', 292.5], ['NW', 315], ['NNW', 337.5],
+    ['S', 0],   ['SSE', 22.5],  ['SE',  45], ['ESE', 67.5],
+    ['E', 90],  ['ENE', 112.5], ['NE', 135], ['NNE', 157.5],
+    ['N', 180], ['NNW', 202.5], ['NW', 225], ['WNW', 247.5],
+    ['W', 270], ['WSW', 292.5], ['SW', 315], ['SSW', 337.5],
   ]
 
   let currentClosest = ['X', 360]
