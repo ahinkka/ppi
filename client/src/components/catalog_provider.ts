947c0ad34b1067a6a902e5ae692ddb0a79960412
Atte Hinkka <atte.hinkka@iki.fi>
2026-01-18 18:55:33 +0200

    Component lifecycle fixes for catalog provider

--- client/src/components/catalog_provider.ts
+++ client/src/components/catalog_provider.ts
@@ -5,11 +5,13 @@ import { ObserverActions, ObserverDispatch } from '../constants'
 type Props = { dispatch: ObserverDispatch, url: string }
 
 class CatalogProvider extends Component<Props> {
-  private intervalId: ReturnType<typeof setInterval> | null
+  private intervalId: number | null
+  private initialTimeoutId: number | null
 
   constructor(props: Readonly<Props> | Props) {
     super(props)
     this.intervalId = null
+    this.initialTimeoutId = null
   }
 
   componentDidMount() {
@@ -24,12 +26,13 @@ class CatalogProvider extends Component<Props> {
         })
     }
 
-    this.intervalId = setInterval(update, 30000)
-    setTimeout(update, 0)
+    this.intervalId = window.setInterval(update, 30000)
+    this.initialTimeoutId = window.setTimeout(update, 0)
   }
 
   componentWillUnmount() {
-    if (this.intervalId) clearInterval(this.intervalId)
+    if (this.intervalId) window.clearInterval(this.intervalId)
+    if (this.initialTimeoutId) window.clearTimeout(this.initialTimeoutId)
   }
 
   render() {
